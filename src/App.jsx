9b505c1c586223d72d420127a5e7cdd27b4b26a0
Xandinho <souzasantosalexandre825@gmail.com>
2025-10-30 10:47:47 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.module.css'
+
+function App() {
+  
+// Aqui vai o Java script
+  return (
+    // aqui é o HTML
+    // aqui dentro do return só pode ter um elemento pai ou seja um elemento só
+    /* Consegue colocar vários elementos gracas as setas o maior e o menor*/
+    <>
+      <h1 className="titulo">Xandão</h1>
+      <p className="descricao">Aqui vai uma descrição sobre o Xandão.</p>
+    </>
+  )
+}
+
+export default App
