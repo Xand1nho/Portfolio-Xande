abb455e35e269fce0809b2877eb7ee8c9a1c8a79
Xandinho <souzasantosalexandre825@gmail.com>
2025-11-03 11:42:03 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import styles from'./App.module.css'
+import perfil from '../public/perfil.jpg'
 
 function App() {
   
@@ -9,20 +10,25 @@ function App() {
     // aqui dentro do return só pode ter um elemento pai ou seja um elemento só
     /* Consegue colocar vários elementos gracas as setas o maior e o menor*/
     <>
-      <h1 className="titulo">Xandão</h1>
-      <p className="descricao">Aqui vai uma descrição sobre o Xandão.</p>
+      <h1 className="titulo">Descrição</h1>
       <nav className={styles.menu}>
-        <a href="#s1">New Cars</a>
-        <a href="#s2">Cars</a>
-        <a href="#s3">Parts & Accessories</a>
-        <a href="#s4">Who We Are</a>
+        <a href="#s1">Quem Sou Eu</a>
+        <a href="#s2">Minhas Habilidades</a>
+        <a href="#s3">Como Eu Aprendo</a>
+        <a href="#s4">Curiosidades</a>
       </nav>
       <main>
-        <section>
-          <h2>Sessao 1</h2>
+        <section className={styles.s1} id ='s1'>
+          <div className={styles.left}>
+            <img className={styles.imgPerfil} src={perfil} alt="Foto Perfil" />
+          </div>
+          <div className={styles.right}>
+            <h2 className={styles.title}>Alexandre Souza</h2>
+            <p className={styles.paragraph}> Informático, Robótico e Desenvolvedor iniciante. Estou a 5 anos no Forja com a Parceria do SENAI, estou aprendendo a programar.</p>
+          </div>
         </section>
 
-        <section>
+        <section >
           <h2>Sessao 2</h2>
         </section>
 
@@ -39,3 +45,4 @@ function App() {
 }
 
 export default App
+
