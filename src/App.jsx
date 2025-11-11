7050e90d23c1b8f3d85377a1ea9c6a583d2777ca
Xandinho <souzasantosalexandre825@gmail.com>
2025-11-11 11:46:55 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,51 @@
 
 import styles from'./App.module.css'
 import perfil from '../public/perfil.jpg'
+import IconInstagram from '../public/Instagram.png'
+import IconFacebook from '../public/Facebook1.png'
+import IconWhatsapp from '../public/Whastapp.png'
+import IconLinkedin from '../public/Linkedin.png'
+import HtmlImage from '/HTML.png'
+import CssImage from '/Cssremovedor.png'
+import JsImage from '/JavaScript.png'
+import ViteImage from '/Vitejs3.png'
+import ReactImage from '/React.png'
+import VercelImage from '/Vercel.png'
+import BairroImage from '/RioBonito.png'
+import TradegiaImage from '/Tragedia.png'
+
+import { useState} from 'react'
 
 function App() {
+  const defaultPhoneNumber = '5541984502786'
+
+  const[formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  })
+
+
+const handleChange = (event) => {
+  const{name, value} = event.target /* Os tres pontos é uma copia do outro form Data */
+  setFormData({...formData, [name]: value}) /*...formData é para manter os outros valores que ja estao preenchidos */ 
+}
+
+
+ // console.log(formData)
+
+  const handleZap = () => {
+  const {name, email, message} = formData
+
+
+  const urlZap = `https://api.whatsapp.com/send?phone=${defaultPhoneNumber}&text=
+  Nome:%20${name}%0D%0A
+  Email:%20${email}%0D%0A
+  Mensagem:%20${message}%0D%0A`
+
+
+  window.open(urlZap, "_blank")
+ }
   
 // Aqui vai o Java script
   return (
@@ -24,22 +67,85 @@ function App() {
           </div>
           <div className={styles.right}>
             <h2 className={styles.title}>Alexandre Souza</h2>
-            <p className={styles.paragraph}> Informático, Robótico e Desenvolvedor iniciante. Estou a 5 anos no Forja com a Parceria do SENAI, estou aprendendo a programar.</p>
+            <p className={styles.paragraph}> Informático, Robótico e Desenvolvedor iniciante. Sou estudante do SENAI PR e estou aprendendo a programação.</p>
           </div>
         </section>
 
-        <section >
-          <h2>Sessao 2</h2>
+        <section className={styles.s2} id ='s2'>
+          <h2 className={styles.tecTitle}>Tecnologias</h2>
+          <div className={styles.wrapCards}>
+              <div className={styles.card}>
+                  <img width={300} src={HtmlImage} alt='HTML' />
+                  <h3>HTML</h3>
+                  <p>HTML é a sigla para HyperText Markup Language que é a linguagem de marcação utilizada para criar páginas na web.</p>  
+              </div>
+              <div className={styles.card}>
+                  <img width={300} src={CssImage} alt='CSS' />
+                  <h3>CSS</h3>
+                  <p>CSS é a sigla para Cascading Style Sheets,
+                     que é a linguagem de estilo utilizada para descrever a apresentação de documentos HTML.</p>
+              </div>
+
+              <div className={styles.card}>
+                <img width={300} src={JsImage} alt='JavaScript' />
+                <h3>JavaScript</h3>
+                <p>JavaScript é uma linguagem de programação que permite implementar funcionalidades complexas em páginas web.</p>
+              </div>
+
+              <div className={styles.card}>
+                  <img width={200} src={ViteImage} alt='VITE' />
+                  <h3>VITE JS</h3>
+                  <p>Vite é uma ferramenta de build e desenvolvimento de front-end que visa melhorar a eficiência do desenvolvimento, tornando-o mais rápido e fácil.</p>
+              </div>
+
+              <div className={styles.card}>
+                  <img width={250} src={ReactImage} alt='REACT' />
+                  <h3>REACT</h3>
+                  <p>React é uma biblioteca JavaScript para construir interfaces de usuário, permitindo a criação de componentes reutilizáveis e uma experiência de usuário mais dinâmica.</p>
+              </div>
+
+              <div className={styles.card}>
+                  <img width={250} src={VercelImage} alt='VERCEL' />
+                  <h3>VERCEL</h3>
+                  <p>Vercel é uma plataforma de desenvolvimento e hospedagem na nuvem que facilita a criação e implantação de aplicações web modernas.</p>
+              </div>
+          </div>
         </section>
 
-        <section>
-          <h2>Sessao 3</h2>
+        <section id='s3' className={styles.s3}>
+          <h2>GitHub Repositorios </h2>
         </section>
 
-        <section>
-          <h2>Sessao 4</h2>
+      <section id='s5' className={styles.meubairro}>
+        <h2>O bairro Rio Bonito na verdade não é o esperado</h2>
+        <div className={styles.bairro}>
+          <img width={400} src={BairroImage} alt="Bairro Rio Bonito" />
+          <img width={400} src={TradegiaImage} alt="Bairro Rio Bonito" />
+          <p>O bairro Rio Bonito em Curitiba, Paraná, Brasil, é conhecido por sua diversidade cultural e história rica. Fundado no início do século XX, o bairro tem raízes que remontam aos imigrantes europeus, especialmente italianos e poloneses, que se estabeleceram na região. Ao longo dos anos, Rio Bonito desenvolveu uma identidade única, combinando tradições antigas com influências modernas.</p>
+          </div>
+          </section>
+
+
+        <section id='s4' className={styles.s4}>
+          <h2>Contato</h2>
+          <div className={styles.formData}>
+            <label htmlFor="name">Informe seu nome</label>
+            <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
+            <label htmlFor="email">Informe seu email</label>
+            <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
+            <label htmlFor="message">Informe uma mensagem</label>
+            <textarea id="message" name="message" value={formData.message} onChange={handleChange} cols="30" rows="10"required></textarea>
+            <button onClick={handleZap}>Enviar</button>
+          </div>
         </section>
       </main>
+      <footer className={styles.rodape}>
+        <p>Desenvolvido por Alexandre Souza</p>
+        <a href='https://www.instagram.com/zxandinhouz/' target='_blank' rel='noopener noreferrer'> <img width={70} src={IconInstagram} alt='Instagram'/> </a>
+        <a href='https://www.facebook.com/' target='_blank' rel='noopener noreferrer'> <img width={70} src={IconFacebook} alt='Facebook'/> </a>
+        <a href='https://www.whatsapp.com/' target='_blank' rel='noopener noreferrer'> <img width={70} src={IconWhatsapp} alt='WhatsApp'/> </a>
+        <a href='https://www.linkedin.com/in/alexandre-souza-santos-b87487395/' target='_blank' rel='noopener noreferrer'> <img width={70} src={IconLinkedin} alt='Linkedin'/> </a>
+      </footer>
     </>
   )
 }
